27cffd3cc0d88aff4cc9d246eb22dd4a727da7b4
Energon5 <computergeek01@gmail.com>
2014-06-10 15:00:04 -0500

    Changes

--- scanandalign.py
+++ scanandalign.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 from Bio.Emboss.Applications import NeedleCommandline
 
 def genscan(acc):
 	# Test feature, see if we can pull the sequence from the site
 	#url = raw_input("Enter a sequence to extract from solgenomics: " + acc)
-
+	acc.strip("'")
 	# Create response object for the solgenomics website, then concatenate the accession number onto the search
 	r  = requests.get("http://solgenomics.net/search/unigene.pl?unigene_id=" + acc)
 
@@ -27,17 +28,30 @@ def genscan(acc):
 		return cleanseq
 	return cleanseq
 
-seq1 = genscan("SGN-U577650")
-seq2 = genscan("214301")
 
-seq1f = open("seq1.fasta", "w")
-seq1f.write(seq1)
-seq1f.close()
+io = pd.read_csv('gongcsv.csv', sep=",", usecols=(0,))
+iolist = map(list, io.values)
 
-seq2f = open("seq2.fasta", "w")
-seq2f.write(seq2)
-seq2f.close()
 
-needle_cline = NeedleCommandline(asequence="seq1.fasta", bsequence="seq2.fasta", gapopen = 10, gapextend = 0.5, outfile = "needle.txt")
-stdout, stderr = needle_cline()
+seqb = iolist[0]
+seqbs = str(seqb).strip('[]')
+seqbs = seqbs.replace("/", '')
+seqa = genscan(seqbs)
+print seqa
+seqbase = open("seq1.fasta", "w")
+seqbase.write(seqbs)
+seqbase.write(seqa)
+seqbase.close()
+
+#seqcompare = open("seq2.fasta", "wb")
+#for nums in iolist:
+#	seq2s = nums.strip('[]')
+#	seq2bs = str(seq2s)
+#	seqcompare.write(seq2bs)
+	
+#	seqcompare.write('\n')
+#seqcompare.close()
+
+#needle_cline = NeedleCommandline(asequence="seq1.fasta", bsequence="seq2.fasta", gapopen = 10, gapextend = 0.5, outfile = "needle.txt")
+#stdout, stderr = needle_cline()
 
--- xlsxextract.py
+++ xlsxextract.py
@@ -1,6 +1,9 @@
 import pandas as pd
 io = pd.read_csv('gongcsv.csv', sep=",", usecols=(0,))
-print io
+iolist = map(list, io.values)
+print iolist[0]
+for nums in iolist:
+	print nums
 #csv = open('gongcsv.csv', 'r')
 #csv.readline().split()
 #print csv
