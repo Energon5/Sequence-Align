374ade34b629ddb861d84ac1a2d63c82bef847a7
Energon5 <computergeek01@gmail.com>
2014-06-09 21:11:20 -0500

    Test

--- emboss.py
+++ emboss.py
@@ -3,5 +3,5 @@ aseq = "alpha.faa"
 bseq = "beta.faa"
 needle_cline = NeedleCommandline(asequence=aseq, bsequence=bseq, gapopen = 10, gapextend = 0.5, outfile = "needle.txt")
 stdout, stderr = needle_cline()
-
+print stdout + stderr
 
