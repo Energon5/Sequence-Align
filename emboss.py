5249edb883fd6ecf2ba01a1642c4cd50cb2b3ceb
Energon5 <computergeek01@gmail.com>
2014-06-09 21:03:35 -0500

    Working Sequence Alignment Script

---
+++ emboss.py
@@ -0,0 +1,7 @@
+from Bio.Emboss.Applications import NeedleCommandline
+aseq = "alpha.faa"
+bseq = "beta.faa"
+needle_cline = NeedleCommandline(asequence=aseq, bsequence=bseq, gapopen = 10, gapextend = 0.5, outfile = "needle.txt")
+stdout, stderr = needle_cline()
+print stdout + stderr
+
